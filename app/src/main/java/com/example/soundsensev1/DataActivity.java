bd46a8769dcba0fed3bf78d1b5917f251cb9e51d
maxb6 <maxburah@gmail.com>
2021-03-10 18:20:32 -0500

    pulled-displayed-user-sensor-data

--- app/src/main/java/com/example/soundsensev1/DataActivity.java
+++ app/src/main/java/com/example/soundsensev1/DataActivity.java
@@ -1,6 +1,7 @@
 package com.example.soundsensev1;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -10,21 +11,29 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataActivity extends AppCompatActivity {
 
     protected ListView sensorListView;
-    protected List<String> sensorList;
-    private DatabaseReference reference;
+    private DatabaseReference sensorReference;
+    private DatabaseReference userReference;
+    private DatabaseReference userSensorReference;
     private String userID;
     private TextView tv5;
+    private ArrayList<String> inputSensorValues = new ArrayList<>();
+    private ArrayList<String> fbSensorValues = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,18 +49,31 @@ public class DataActivity extends AppCompatActivity {
 
         //load sensor value
         sensorListView = findViewById(R.id.sensorListView);
-        tv5 = findViewById(R.id.textView5);
-        loadSensorValues();
+        userReference = FirebaseDatabase.getInstance().getReference("Users")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("sensorValues");
+        storeUserSensorValues();
+        printUserSensorValues();
+
     }
 
-    protected void loadSensorValues() {
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-        reference = FirebaseDatabase.getInstance().getReference().child("Sensor");
-        reference.addValueEventListener(new ValueEventListener() {
+    }
+
+    protected void storeUserSensorValues() {
+
+        //reference to firebase to retrieve sensor data
+        sensorReference = FirebaseDatabase.getInstance().getReference().child("Sensor");
+        sensorReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 String value = snapshot.child("Analog").getValue().toString();
-                tv5.setText(value);
+                //add sensor value to an arraylist
+                inputSensorValues.add(value);
+                //upload array list to firebase database
+                userReference.setValue(inputSensorValues);
             }
 
             @Override
@@ -60,26 +82,47 @@ public class DataActivity extends AppCompatActivity {
             }
         });
 
-        /*
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sensorList);
-        sensorListView.setAdapter(adapter);
-        reference.addValueEventListener(new ValueEventListener() {
+    }
+
+    protected void printUserSensorValues(){
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fbSensorValues);
+
+        //retrieve sensorvalues for specific user from firebase
+        //display these values in a list view
+        userReference.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                sensorList.clear();
-                for(DataSnapshot snapshot1 : snapshot.getChildren()){
-                    sensorList.add(snapshot1.getValue().toString());
-                }
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String currentSensorValue = snapshot.getValue(String.class);
+                fbSensorValues.add(currentSensorValue);
                 adapter.notifyDataSetChanged();
             }
 
             @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(DataActivity.this,"Sensor Value error!",Toast.LENGTH_LONG).show();
+
             }
         });
+        sensorListView.setAdapter(adapter);
 
-         */
     }
+
 
 }
