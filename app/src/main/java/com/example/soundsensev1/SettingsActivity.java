8834decad6e5715bcf8a1b157e537cd388930b9f
maxb6 <maxburah@gmail.com>
2021-03-19 15:58:53 -0400

    #25-implemented-on-off-setting

--- app/src/main/java/com/example/soundsensev1/SettingsActivity.java
+++ app/src/main/java/com/example/soundsensev1/SettingsActivity.java
@@ -1,11 +1,24 @@
 package com.example.soundsensev1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Switch;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private DatabaseReference sensorControlReference;
+    private Switch controlSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +31,49 @@ public class SettingsActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Settings");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //control switch initialization
+        controlSwitch = findViewById(R.id.controlSwitch);
+        sensorControlReference = FirebaseDatabase.getInstance().getReference().child("Device").child("ON&OFF");
+
+        setControlSwitchValue();
+
+    }
+
+    protected void setControlSwitchValue(){
+        sensorControlReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //read the current firebase value of device and convert it to int
+                String controlString = snapshot.getValue().toString();
+                int controlInt = Integer.parseInt(controlString);
+                Log.i("Settings","control switch: "+controlInt);
+                //set the switch to whatever the current firebase value is
+                if(controlInt==0){
+                    controlSwitch.setChecked(false);
+                }else{
+                    controlSwitch.setChecked(true);
+                }
+
+                //when user activates control switch, set firebase value to 1 which will allow sensor to send values
+                //otherwise it will be turned off and it wont send values
+
+                controlSwitch.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(controlSwitch.isChecked()){
+                            sensorControlReference.setValue(1);
+                        }
+                        else{
+                            sensorControlReference.setValue(0);
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
