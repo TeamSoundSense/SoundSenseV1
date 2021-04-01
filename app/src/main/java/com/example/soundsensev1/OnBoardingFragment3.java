fb458e895554c4f7f1e6cd76c2e5508c28ff6608
Jacob <jacobrodolfo1999@gmail.com>
2021-03-31 20:44:28 -0400

    Added Introductory activity with sliders, the only thing would be changing the information in the sliders

---
+++ app/src/main/java/com/example/soundsensev1/OnBoardingFragment3.java
@@ -0,0 +1,36 @@
+package com.example.soundsensev1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class OnBoardingFragment3 extends Fragment {
+
+    FloatingActionButton arrowButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup)inflater.inflate(R.layout.fragment_on_boarding3, container, false);
+
+        arrowButton = root.findViewById(R.id.arrowButton);
+
+        arrowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return root;
+    }
+}
